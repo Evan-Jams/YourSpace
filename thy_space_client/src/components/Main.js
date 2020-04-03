bb7f5848a26688d660609b7b3aa4740c19452f67
Nathanael Fletcher <nathanaelfletcher19@gmail.com>
2020-04-03 16:58:30 -0400

    moved main stuff

--- thy_space_client/src/components/Main.js
+++ thy_space_client/src/components/Main.js
@@ -1,9 +1,43 @@
 import React, {Component} from 'react'
 
 class Main extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      posts: []
+    }
+  }
+
+  async createPost(formData) {
+    let response = await fetch (`http://localhost:3000/`, {
+      body: JSON.stringify(formData),
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json, text/plain, */*',
+        'Content-Type': 'application/json'
+      }
+    })
+    let data = await response.json()
+    console.log(data)
+    this.setState(prevState => {
+      return({ users: [...prevState.users, data]})
+    })
+    this.login(data.username)
+  }
+
+
   render(){
     return(
-      <div>Main</div>
+      <div>
+        <div>
+          <button>Make ye a post</button>
+        </div>
+        <div>
+          {this.state.posts.map(post => (
+            <Post post={post} />
+          ))}
+        </div>
+      </div>
     )
   }
 }
