4bef65a95e52f88266ddfc23655f82f05b90e00c
Nathanael Fletcher <nathanaelfletcher19@gmail.com>
2020-04-03 16:44:30 -0400

    worked on main

--- thy_space_client/src/components/Main.js
+++ thy_space_client/src/components/Main.js
@@ -8,6 +8,23 @@ class Main extends Component {
     }
   }
 
+  async createPost(formData) {
+    let response = await fetch (`http://localhost:3000/posts`, {
+      body: JSON.stringify(formData),
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json, text/plain, */*',
+        'Content-Type': 'application/json'
+      }
+    })
+    let data = await response.json()
+    console.log(data)
+    this.setState(prevState => {
+      return({ users: [...prevState.users, data]})
+    })
+    this.login(data.username)
+  }
+
 
   render(){
     return(
